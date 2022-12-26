2f31820ae9456ef100a0953d98f65dd6487ce612
Lucas Souza <los799@live.com>
2022-12-26 19:06:02 -0300

    feat(translations): add ptBR translation
    So, I was in needed and I've created this translation. Decided to open a PR, since its pretty simple and can be useful for someone else.

---
+++ src/lang/pt-BR.js
@@ -0,0 +1,104 @@
+export default {
+    undo: {
+        toolbar: 'Desfazer',
+    },
+    redo: {
+        toolbar: 'Refazer',
+    },
+    clear: {
+        toolbar: 'Limpar',
+    },
+    h: {
+        toolbar: 'Cabeçalhos',
+    },
+    h1: {
+        toolbar: 'Cabeçalho 1',
+        placeholder: 'Cabeçalho 1',
+    },
+    h2: {
+        toolbar: 'Cabeçalho 2',
+        placeholder: 'Cabeçalho 2',
+    },
+    h3: {
+        toolbar: 'Cabeçalho 3',
+        placeholder: 'Cabeçalho 3',
+    },
+    h4: {
+        toolbar: 'Cabeçalho 4',
+        placeholder: 'Cabeçalho 4',
+    },
+    h5: {
+        toolbar: 'Cabeçalho 5',
+        placeholder: 'Cabeçalho 5',
+    },
+    h6: {
+        toolbar: 'Cabeçalho 6',
+        placeholder: 'Cabeçalho 6',
+    },
+    bold: {
+        toolbar: 'Negrito',
+        placeholder: 'Negrito',
+    },
+    italic: {
+        toolbar: 'Itálico',
+        placeholder: 'Itálico',
+    },
+    strikethrough: {
+        toolbar: 'Tachado',
+        placeholder: 'Tachado',
+    },
+    quote: {
+        toolbar: 'Citação',
+        placeholder: 'Citação',
+    },
+    ul: {
+        toolbar: 'Lista desordenada',
+        placeholder: 'Lista desordenada',
+    },
+    ol: {
+        toolbar: 'Lista ordenada',
+        placeholder: 'Lista ordenada',
+    },
+    table: {
+        toolbar: 'Adicionar tabela',
+    },
+    hr: {
+        toolbar: 'Linha',
+    },
+    link: {
+        toolbar: 'Inserir link',
+        descPlaceholder: 'Link',
+    },
+    image: {
+        toolbar: 'Inserir imagem',
+    },
+    imageLink: {
+        toolbar: 'Inserir link',
+    },
+    uploadImage: {
+        toolbar: 'Upload de imagem',
+    },
+    code: {
+        toolbar: 'Inserir bloco de código',
+    },
+    save: {
+        toolbar: 'Salvar',
+    },
+    preview: {
+        enabled: 'Habilitar pré-visualização',
+        disabled: 'Desabilitar pré-visualização',
+    },
+    toc: {
+        title: 'Navegação do diretório',
+        enabled: 'Habilitar TOC',
+        disabled: 'Desabilitar TOC',
+    },
+    syncScroll: {
+        enabled: 'Habilitar sincronização de rolagem',
+        disabled: 'Desabilitar sincronização de rolagem',
+    },
+    fullscreen: {
+        enabled: 'Tela cheia (Pressione ESC para sair)',
+        disabled: 'Sair da Tela Cheia',
+    },
+};
